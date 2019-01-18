6d38ba1287add6e0a8add0bdd5d269558c93dbd7
Kishan <kkpatel911@gmail.com>
2019-01-18 16:36:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+var scores, rountScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function btn() {
+    // console.log('H');
+    //Do something
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // Display the result
+    var diceDom = document.querySelector('.dice');
+
+    diceDom.style.display = 'block';
+    diceDom.src = "dice-" + dice + '.png';
+
+
+    //If statement
+    if (dice !== 1) {
+        //ADD SCORE
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;;
+    } else {
+        //Next Player
+        if (activePlayer === 0) {
+            activePlayer = 1;
+            roundScore = 0;
+        } else {
+            activePlayer = 0;
+            roundScore = 0;
+        }
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
