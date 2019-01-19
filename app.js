20913d88e1fac411e96f26522887133c51d97f60
Kishan <kkpatel911@gmail.com>
2019-01-19 14:03:12 -0500

    added HOLD btn function

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ activePlayer = 0;
 
 
 
+
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 
 // var x = document.querySelector('#score-0').textContent;
@@ -47,34 +48,60 @@ document.querySelector('.btn-roll').addEventListener('click', function btn() {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;;
     } else {
-        //Next Player
-        if (activePlayer === 0) {
-            activePlayer = 1;
-            roundScore = 0;
-        } else {
-            activePlayer = 0;
-            roundScore = 0;
-        }
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
+        nextPlayer();
     }
 
 });
 
 
 
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    //Add current score to global score
+
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+    console.log(roundScore)
+
+    //update the UI
+
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+
+    //Check if player won the game
+
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = "Winner!";
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    } else {
+        nextPlayer();
+    }
+
+    //Next Player
+
+});
+
+function nextPlayer() {
+    //Next Player
+    if (activePlayer === 0) {
+        activePlayer = 1;
+        roundScore = 0;
+    } else {
+        activePlayer = 0;
+        roundScore = 0;
+    }
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
 
 
 
-
-
-
-
-
+}
 
